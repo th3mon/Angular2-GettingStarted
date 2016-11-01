2972078d50fa0c3da181481ad51a7d0e4c008c87
th3mon <p.szelenberger@gmail.com>
2016-11-01 17:22:10 +0100

    Data Binding & Pipes

--- APM - Start/app/products/product-list.component.ts
+++ APM - Start/app/products/product-list.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
     products: any[] = [
         {
             "productId": 1,
@@ -58,5 +62,9 @@ export class ProductListComponent {
             "starRating": 4.6,
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
         }
-    ]
+    ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
