69d20072aee1b2bbd3b994a2aca976ad222bf54a
th3mon <p.szelenberger@gmail.com>
2016-11-02 00:12:40 +0100

    Angular Modules

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -1,47 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { WelcomeComponent }  from './home/welcome.component';
-
-import { ProductListComponent }  from './products/product-list.component';
-import { ProductDetailComponent }  from './products/product-detail.component';
-import { ProductGuardService }  from './products/product-guard.service';
-import { ProductFilterPipe } from './products/product-filter.pipe';
-import { StarComponent } from './shared/star.component';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'product/:id',
-        canActivate: [ ProductGuardService ],
-        component: ProductDetailComponent },
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
-  ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        RouterModule.forRoot([
+            { path: 'welcome', component: WelcomeComponent },
+            { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+            { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+        ]),
+        ProductModule
+    ],
 
-  declarations: [
-    AppComponent,
-    WelcomeComponent,
-    ProductListComponent,
-    ProductDetailComponent,
-    ProductFilterPipe,
-    StarComponent
-  ],
+    declarations: [
+        AppComponent,
+        WelcomeComponent
+    ],
 
-  providers: [
-    ProductGuardService
-  ],
-
-  bootstrap: [ AppComponent ]
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- APM - Start/app/products/product-guard.service.ts
+++ APM - Start/app/products/product-guard.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router, ActivatedRouteSnapshot, CanActivate } from '@angular/router';
 
 @Injectable()
-export class ProductGuardService implements CanActivate {
+export class ProductDetailGuard implements CanActivate {
     constructor(private _router: Router) {}
 
     canActivate(route: ActivatedRouteSnapshot): boolean {
